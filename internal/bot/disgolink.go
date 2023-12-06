d95e54cf22d399b47a50f4819efc6aa0391cfc42
Razvan-Antonio Berbece <sc19rab@leeds.ac.uk>
2023-12-06 20:13:05 +0000

    if this fixes it...

--- internal/bot/disgolink.go
+++ internal/bot/disgolink.go
@@ -7,9 +7,12 @@ import (
 	"strconv"
 
 	"github.com/disgoorg/disgolink/v3/disgolink"
+	"github.com/joho/godotenv"
 )
 
 var (
+	_ = godotenv.Load(".env")
+
 	NodeName      = os.Getenv("LAVALINK_NODE_NAME")
 	NodeAddress   = os.Getenv("LAVALINK_NODE_ADDRESS")
 	NodePassword  = os.Getenv("LAVALINK_NODE_PASSWORD")
